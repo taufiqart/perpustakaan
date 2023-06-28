25b84e578ab23224e7f6e3d525738cf6327756e5
Atoilah <atoilahputra016@gmail.com>
2023-06-28 11:57:30 +0700

    navbar oke bubble chat, slider ga oke

--- perpus/resources/js/Components/landingpage/Dropdown.jsx
+++ perpus/resources/js/Components/landingpage/Dropdown.jsx
@@ -1,46 +1,79 @@
 import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 import { useState } from "react";
+import CButton from "./button";
 
 function Dropdown() {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="relative flex flex-col items-center w-full md:rounded-xl">
-            <button
-                onMouseEnter={() => setIsOpen((prev) => !prev)}
-                className="relative inline-flex items-center justify-center px-5 py-1 overflow-hidden font-medium transition duration-300 ease-out rounded-full hover:shadow-xl group hover:ring-1 hover:ring-green-500"
+        // <div className="relative flex flex-col items-center w-full md:rounded-xl">
+        //     <button
+        //         onMouseEnter={() => setIsOpen((prev) => !prev)}
+        //         className="relative inline-flex items-center justify-center px-5 py-1 overflow-hidden font-medium transition duration-300 ease-out rounded-full hover:shadow-xl group hover:ring-1 hover:ring-green-500"
+        //     >
+        //         <span class="absolute inset-0 w-full h-full group-hover:bg-gradient-to-br from-blue-400 to-green-400"></span>
+        //         <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-90 translate-x-24 group-hover:bg-green-500 rounded-full opacity-80 group-hover:rotate-45 ease"></span>
+        //         <span class="relative text-black group-hover:text-white">
+        //             Beranda
+        //         </span>
+        //         {!isOpen ? (
+        //             <AiOutlineCaretDown className=" relative group-hover:text-white h-4" />
+        //         ) : (
+        //             <AiOutlineCaretUp className="relative group-hover:text-white h-4" />
+        //         )}
+        //     </button>
+
+        //     {isOpen && (
+        //         <div className="backdrop-blur-sm  absolute top-11">
+        //             <ul className="block">
+        //                 <li className="group">
+        //                     <a
+        //                         href="/"
+        //                         className="text-base bg-white  text-black active:bg-blue-400 hover:bg-green-400 hover:drop-shadow-xl hover:text-white focus:ring focus:ring-green-300 duration-400 py-2 px-7 mx-2 my-[1px] flex"
+        //                     >
+        //                         Beranda
+        //                     </a>
+        //                 </li>
+        //                 <li className="group">
+        //                     <a
+        //                         href="/"
+        //                         className="text-base bg-white  text-black active:bg-blue-400 hover:bg-green-400 hover:drop-shadow-xl hover:text-white focus:ring focus:ring-green-300 duration-400 py-2 px-7 mx-2 my-[1px] flex"
+        //                     >
+        //                         Beranda
+        //                     </a>
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //     )}
+        // </div>
+
+        <div className="block w-full md:w-24">
+            <a
+                onClick={() => setIsOpen((prev) => !prev)}
+                class="relative w-full md:w-24 inline-flex items-center justify-center px-4 py-1 overflow-hidden font-medium transition duration-300 ease-out md:rounded-full hover:shadow-xl group hover:ring-1 hover:ring-green-500"
             >
                 <span class="absolute inset-0 w-full h-full group-hover:bg-gradient-to-br from-blue-400 to-green-400"></span>
                 <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-90 translate-x-24 group-hover:bg-green-500 rounded-full opacity-80 group-hover:rotate-45 ease"></span>
                 <span class="relative text-black group-hover:text-white">
-                    Beranda
+                    Drop
                 </span>
                 {!isOpen ? (
                     <AiOutlineCaretDown className=" relative group-hover:text-white h-4" />
                 ) : (
                     <AiOutlineCaretUp className="relative group-hover:text-white h-4" />
                 )}
-            </button>
-
+            </a>
             {isOpen && (
-                <div className="backdrop-blur-sm  absolute top-11">
-                    <ul className="block">
-                        <li className="group">
-                            <a
-                                href="/"
-                                className="text-base bg-white  text-black active:bg-blue-400 hover:bg-green-400 hover:drop-shadow-xl hover:text-white focus:ring focus:ring-green-300 duration-400 py-2 px-7 mx-2 my-[1px] flex"
-                            >
-                                Beranda
-                            </a>
-                        </li>
-                        <li className="group">
-                            <a
-                                href="/"
-                                className="text-base bg-white  text-black active:bg-blue-400 hover:bg-green-400 hover:drop-shadow-xl hover:text-white focus:ring focus:ring-green-300 duration-400 py-2 px-7 mx-2 my-[1px] flex"
-                            >
-                                Beranda
-                            </a>
-                        </li>
-                    </ul>
+                <div className="block md:absolute md:flex md:flex-col">
+                    <a
+                        href="#_"
+                        class="relative w-full md:w-24 inline-flex items-center justify-center px-4 py-1 overflow-hidden font-medium transition duration-300 ease-out md:rounded-full hover:shadow-xl group hover:ring-1 hover:ring-green-500"
+                    >
+                        <span class="absolute inset-0 w-full h-full group-hover:bg-gradient-to-br from-blue-400 to-green-400"></span>
+                        <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-90 translate-x-24 group-hover:bg-green-500 rounded-full opacity-80 group-hover:rotate-45 ease"></span>
+                        <span class="relative text-black group-hover:text-white">
+                            Isi
+                        </span>
+                    </a>
                 </div>
             )}
         </div>
--- perpus/resources/js/Components/landingpage/Navbar.jsx
+++ perpus/resources/js/Components/landingpage/Navbar.jsx
@@ -1,8 +1,7 @@
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import CButton from "./button";
-import Dropdown from "../Dropdown";
-
+import Dropdown from "./Dropdown";
 export default function Navbar() {
     const navRef = useRef();
 
@@ -14,11 +13,12 @@ export default function Navbar() {
         <header className="flex items-center h-14 justify-between md:justify-center z-10 backdrop-blur-lg shadow-lg top-0 px-5 sticky">
             <h3 className="font-bold text-xl md:hidden">Skensa</h3>
             <nav
-                className="fixed flex flex-col justify-center items-center gap-1 w-full h-[100vh] md:h-full backdrop-blur-lg bg-gray-300 bg-opacity-60 md:bg-transparent left-0 -top-[100vh] md:top-0 md:flex md:flex-row  duration-1000"
+                className="fixed flex flex-col justify-center items-center gap-1 w-full h-screen md:h-full backdrop-blur-lg bg-white bg-opacity-60  left-0 -top-[100vh] md:top-0 md:flex md:flex-row  duration-1000"
                 ref={navRef}
             >
                 <CButton />
                 <CButton />
+                <Dropdown />
                 <CButton />
                 <button
                     className="absolute top-2 right-2 md:hidden"
