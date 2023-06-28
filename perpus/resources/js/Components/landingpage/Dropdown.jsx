58c72fedacf97f8350a1d2012574f2ba9eb355c3
Atoilah <atoilahputra016@gmail.com>
2023-06-28 12:25:21 +0700

    sisa slider

--- perpus/resources/js/Components/landingpage/Dropdown.jsx
+++ perpus/resources/js/Components/landingpage/Dropdown.jsx
@@ -66,13 +66,31 @@ function Dropdown() {
                 <div className="block md:absolute md:flex md:flex-col">
                     <a
                         href="#_"
-                        class="relative w-full md:w-24 inline-flex items-center justify-center px-4 py-1 overflow-hidden font-medium transition duration-300 ease-out md:rounded-full hover:shadow-xl group hover:ring-1 hover:ring-green-500"
+                        class="relative bg-white w-full md:w-24 inline-flex items-center justify-center px-4 py-1 font-medium duration-200 md:rounded-tr-sm hover:shadow-xl group hover:ring-1 hover:bg-green-400"
                     >
-                        <span class="absolute inset-0 w-full h-full group-hover:bg-gradient-to-br from-blue-400 to-green-400"></span>
-                        <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-90 translate-x-24 group-hover:bg-green-500 rounded-full opacity-80 group-hover:rotate-45 ease"></span>
-                        <span class="relative text-black group-hover:text-white">
-                            Isi
-                        </span>
+                        <svg
+                            className="absolute -left-3 top-0 fill-white group-hover:fill-green-400 -z-10"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="23"
+                            height="16"
+                            viewBox="0 0 23 16"
+                            fill="none"
+                        >
+                            <path d="M0.750831 0.0224888L22.738 -0.000979136L16.2414 15.6263L0.750831 0.0224888Z" />
+                        </svg>
+                        issi
+                    </a>
+                    <a
+                        href="#_"
+                        class="relative bg-white w-full md:w-24 inline-flex items-center justify-center px-4 py-1 font-medium duration-200 md:rounded-none hover:shadow-xl group hover:ring-1 hover:bg-green-400"
+                    >
+                        issi
+                    </a>
+                    <a
+                        href="#_"
+                        class="relative bg-white w-full md:w-24 inline-flex items-center justify-center px-4 py-1 font-medium duration-200 md:rounded-b-sm hover:shadow-xl group hover:ring-1 hover:bg-green-400"
+                    >
+                        issi
                     </a>
                 </div>
             )}
