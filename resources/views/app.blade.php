635e6e2c0edcccc9e2da5a43ca75f0b6b1be3ad0
taufiqart <taufiqqurrohman333@gmail.com>
2024-03-17 11:34:50 +0700

    :tada: add metadata

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="icon" href="/assets/icons/logo.png" type="image/png" sizes="16x16">
+    {!! \App\Meta::render() !!}
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
