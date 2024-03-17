3b2bd75f1afa1e371592e8bfc226118b62c241d2
taufiqart <taufiqqurrohman333@gmail.com>
2024-03-17 11:13:28 +0700

    :tada: add metadata

---
+++ app/Helpers/Metadata.php
@@ -0,0 +1,45 @@
+<?php
+
+function MetadataPerpus()
+{
+    return [
+        "url" => env("APP_URL"),
+        "title" => [
+            "Perpustakaan SMKN 1 Pasuruan",
+            "Perpustakaan SMKN 1 Pasuruan"
+        ],
+        "description" => [
+            "Portal Perpustakaan SMKN 1 Pasuruan",
+            "Portal Perpustakaan SMKN 1 Pasuruan"
+        ],
+        "keywords" => [
+            "perpustakaan,article,portal,info"
+        ],
+        "type" => [
+            "article"
+        ]
+    ];
+}
+function MetadataSitusiba()
+{
+    return [
+        "url" => env("APP_URL"),
+        "title" => [
+            "SITU SIBA",
+            "SITU SIBA Silahkan Tulis Silahkan Baca"
+        ],
+        "description" => [
+            "Selamat datang di SITU SIBA disini kami menyediakan platform bagi siswa siswi SMKN 1 Pasuruan 
+            agar Karya Tulis seperti Cerpen, Novel ataupun karya ilmiah agar dapat dipublish dan dibaca oleh umum.",
+
+            "Selamat datang di SITU SIBA disini kami menyediakan platform bagi siswa siswi SMKN 1 Pasuruan 
+            agar Karya Tulis seperti Cerpen, Novel ataupun karya ilmiah agar dapat dipublish dan dibaca oleh umum."
+        ],
+        "keywords" => [
+            "baca,membaca,article,karya,karya tulis,karya ilmiah"
+        ],
+        "type" => [
+            "article"
+        ]
+    ];
+}
