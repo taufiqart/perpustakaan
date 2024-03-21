3070650397eab07acf3330cfb5343a61b8ac5bf3
taufiqart <taufiqqurrohman333@gmail.com>
2024-03-21 20:12:38 +0700

    :tada: modify view

--- resources/js/Pages/admin/Paper/View.jsx
+++ resources/js/Pages/admin/Paper/View.jsx
@@ -1,16 +1,29 @@
 import { Head, Link, useForm, usePage } from "@inertiajs/react";
 import AdminLayout from "@/Layouts/admin/AdminLayout";
-import BookView from "../../../Components/BookView";
-import { useState,useEffect } from "react";
-import Modal from '@/Components/Modal';
-import BookLoader from '@/Components/BookLoader';
-import Alert from '@/Components/Alert';
-import SecondaryButton from '@/Components/SecondaryButton';
-import DangerButton from '@/Components/DangerButton';
+import Modal from "@/Components/Modal";
+import BookLoader from "@/Components/BookLoader";
+import Alert from "@/Components/Alert";
+import SecondaryButton from "@/Components/SecondaryButton";
+import DangerButton from "@/Components/DangerButton";
+
+import { useCallback, useState, useEffect } from "react";
+
+import ContentView from "@/Components/shared/ContentView";
 
 export default function View({ paper }) {
     const [confirmDelete, setConfirmDelete] = useState(false);
     const [flash, setFlash] = useState();
+
+    const [file, setFile] = useState(null);
+    const [numPages, setNumPages] = useState();
+    const [containerRef, setContainerRef] = useState(null);
+    const [containerWidth, setContainerWidth] = useState();
+
+    useEffect(() => {
+        paper?.post_assets &&
+            paper?.post_assets.length > 0 &&
+            setFile(paper?.post_assets[0]?.url);
+    }, []);
 
     const onCloseModalDelete = () => {
         setConfirmDelete(false);
@@ -20,7 +33,7 @@ export default function View({ paper }) {
     };
 
     const onOpenModalDelete = (paper) => {
-        setData("slug",paper);
+        setData("slug", paper);
         setConfirmDelete(true);
     };
 
@@ -48,7 +61,7 @@ export default function View({ paper }) {
         <>
             <Head title={`${paper.title}`} />
             <AdminLayout>
-                <div className="w-full flex gap-x-3 p-4 pt-0 -mt-5 cursor-pointer">
+                <div className="w-full flex gap-x-3 p-4 pt-0 md:-mt-14 cursor-pointer">
                     <Link
                         href={route("paper.index")}
                         className="inline-flex items-center px-4 py-2 bg-blue-400 border border-transparent rounded-md font-semibold 
@@ -76,9 +89,16 @@ export default function View({ paper }) {
                         <i className="fas fa-trash"></i> Hapus
                     </div>
                 </div>
-                <div className="flex gap-4 items-start flex-wrap justify-center">
-                    <BookView paper={paper} />
+                <div className="mt-12 md:mt-20 -mx-4 md:mx-0">
+                    <ContentView
+                        content={paper}
+                        fileList={paper?.post_assets}
+                        innerScroll={true}
+                        height="670px"
+                    />
+                    <div className="h-40"></div>
                 </div>
+
                 <Modal
                     show={confirmDelete}
                     onClose={onCloseModalDelete}
--- resources/js/Pages/situsiba/Show.jsx
+++ resources/js/Pages/situsiba/Show.jsx
@@ -1,124 +1,19 @@
-import SitusibaLayout from "@/Layouts/SitusibaLayout";
+import React from "react";
+
 import { Head } from "@inertiajs/react";
-import BookViewCustom from "@/Components/BookViewCustom";
-import CardBox from "../../Components/CardBox";
-
-import { useCallback, useState } from "react";
-import { useResizeObserver } from "@wojtekmaj/react-hooks";
-import { pdfjs, Document, Page } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
-import FilePreviewShow from "@/Components/admin/FilePreviewShow";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    "/public/assets/pdfjs-dist/build/pdf.worker.js",
-    import.meta.url
-).toString();
-
-const options = {
-    cMapUrl: "/public/assets/pdfjs-dist/web/cmaps/",
-    standardFontDataUrl: "/public/assets/pdfjs-dist/web/standard_fonts/",
-};
-
-const resizeObserverOptions = {};
-
-const maxWidth = 1080;
+import SitusibaLayout from "@/Layouts/SitusibaLayout";
+import ContentView from "@/Components/shared/ContentView";
 
 export default function Show({ paper }) {
-    const [file, setFile] = useState(paper?.post_assets[0]?.url);
-    const [numPages, setNumPages] = useState();
-    const [containerRef, setContainerRef] = useState(null);
-    const [containerWidth, setContainerWidth] = useState();
-
-    const onResize = useCallback((entries) => {
-        const [entry] = entries;
-
-        if (entry) {
-            setContainerWidth(entry.contentRect.width);
-        }
-    }, []);
-
-    useResizeObserver(containerRef, resizeObserverOptions, onResize);
-
-    function onFileChange(event) {
-        const { files } = event.target;
-
-        if (files && files[0]) {
-            setFile(files[0] || null);
-        }
-    }
-
-    function onDocumentLoadSuccess({ numPages: nextNumPages }) {
-        setNumPages(nextNumPages);
-    }
-
     return (
         <>
             <SitusibaLayout situsiba={true}>
                 <Head title={"Situ Siba"} />
-                <div className="min-h-screen flex flex-col justify-center items-center px-3 md:px-14 transition-all duration-500">
-                    <div className="-mt-14 md:mt-52 w-full lg:max-w-[50vw] transition-all duration-500">
-                        <BookViewCustom
-                            paper={paper}
-                            height="100%"
-                            width="100%"
-                        >
-                            {
-                                <div
-                                    className="w-full h-full"
-                                    dangerouslySetInnerHTML={{
-                                        __html: paper?.content,
-                                    }}
-                                ></div>
-                            }
-                            {paper?.post_assets &&
-                                paper?.post_assets.length > 0 && (
-                                    <div
-                                        className="-mx-5 sm:-mx-10 md:-mx-[25px]"
-                                        ref={setContainerRef}
-                                    >
-                                        <Document
-                                            file={file}
-                                            onLoadSuccess={
-                                                onDocumentLoadSuccess
-                                            }
-                                            options={options}
-                                            className={
-                                                "w-full h-full flex flex-col items-center"
-                                            }
-                                        >
-                                            {Array.from(
-                                                new Array(numPages),
-                                                (el, index) => (
-                                                    <Page
-                                                        className={
-                                                            "!w-full !object-contain !min-w-full !max-w-full"
-                                                        }
-                                                        key={`page_${
-                                                            index + 1
-                                                        }`}
-                                                        pageNumber={index + 1}
-                                                        width={
-                                                            containerWidth
-                                                                ? Math.min(
-                                                                      containerWidth,
-                                                                      maxWidth
-                                                                  )
-                                                                : maxWidth
-                                                        }
-                                                    />
-                                                )
-                                            )}
-                                        </Document>
-                                    </div>
-                                )}
-                        </BookViewCustom>
-                    </div>
-                    <div className="mt-14 w-full lg:max-w-[50vw] transition-all duration-500">
-                        <CardBox className="[box-shadow:36px_36px_50px_15px_#eed7d1d1] p-10 rounded-[25px]">
-                            <FilePreviewShow dataFiles={paper?.post_assets} />
-                        </CardBox>
-                    </div>
+                <div className="min-h-screen md:mt-52">
+                    <ContentView
+                        fileList={paper?.post_assets}
+                        content={paper}
+                    />
                     <div className="h-96"></div>
                 </div>
             </SitusibaLayout>
